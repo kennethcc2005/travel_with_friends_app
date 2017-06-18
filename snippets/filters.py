b1703fcfd8495a8da197aae33b20b6609810001b
Zoesh <zoesh@Zoeshs-MacBook-Pro.local>
2017-06-17 23:35:53 -0500

    rm snippets and accounts folder

--- snippets/filters.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-from models import Town
-class ProductFilter(django_filters.FilterSet):
-    # min_price = django_filters.NumberFilter(name="price", lookup_type='gte')
-    # max_price = django_filters.NumberFilter(name="price", lookup_type='lte')
-    class Meta:
-        model = Product
-        fields = ['county', 'state', 'n_days']
